ed20c4ec5c05d4650bb7fc859cab250d77bdb8d2
akilann <akilan.narayanaswamy@geappliances.com>
2018-07-19 11:46:34 -0400

    add comments with output

--- 1_Types.lua
+++ 1_Types.lua
@@ -8,31 +8,31 @@ function typePrinter(name, variable)
   print("Type of " .. name .. ": " .. type(variable))
 end
 
-typePrinter("`null`", null)
-typePrinter("`boolean`", boolean)
-typePrinter("`number`", number)
-typePrinter("`string`", string)
-typePrinter("`table`", table)
-typePrinter("`typePrinter`", typePrinter)
+typePrinter("`null`", null) -- Type of `null`: nil
+typePrinter("`boolean`", boolean) -- Type of `boolean`: Boolean
+typePrinter("`number`", number) -- Type of `number`: number
+typePrinter("`string`", string) -- Type of `string`: string
+typePrinter("`table`", table) -- Type of `table`: table
+typePrinter("`typePrinter`", typePrinter) -- Type of `typePrinter`: function
 
 io.read()
 table = 12345
-typePrinter("redefined `table`", table)
+typePrinter("redefined `table`", table) -- Type of redefined `table`: number
 
 io.read()
 num1 = "1"
 num2 = "2"
 num_result = num1 + num2
-typePrinter("`num1`", num1)
-typePrinter("`num2`", num2)
-typePrinter("`num_result`", num_result)
-print(num1 .. " + " .. num2 .. " = " .. num_result)
+typePrinter("`num1`", num1) -- Type of `num1`: string
+typePrinter("`num2`", num2) -- Type of `num2`: string
+typePrinter("`num_result`", num_result) -- Type of `num_result`: num_result
+print(num1 .. " + " .. num2 .. " = " .. num_result) -- 1 + 2 = 3.0
 
 io.read()
 num1 = "1"
 num2 = 2
 num_result = num1 + num2
-typePrinter("`num1`", num1)
-typePrinter("`num2`", num2)
-typePrinter("`num_result`", num_result)
-print(num1 .. " + " .. num2 .. " = " .. num_result)
+typePrinter("`num1`", num1) -- Type of `num1`: string
+typePrinter("`num2`", num2) -- Type of `num2`: number
+typePrinter("`num_result`", num_result) -- Type of `num2`: number
+print(num1 .. " + " .. num2 .. " = " .. num_result) -- 1 + 2 = 3.0
--- 2_Basics.lua
+++ 2_Basics.lua
@@ -1,4 +1,3 @@
-
 -- Variables
 io.read()
 print("Variables")
@@ -6,13 +5,13 @@ print("Variables")
 -- Variables are global by default
 x = 7
 y = 8
-print("x is: " .. x)
-print("y is: " .. y)
+print("x is: " .. x) -- x is: 7
+print("y is: " .. y) -- y is: 8
 
 --[[
   C: { code_block }
   Lua: do code_block end
-]]--
+]]
 
 do
   -- Must explicitly declare local variable
@@ -21,33 +20,33 @@ do
   y = 22
   io.read()
 
-  print("x is now: " .. x)
-  print("y is now: " .. y)
+  print("x is now: " .. x) -- x is now: 21
+  print("y is now: " .. y) -- y is now: 22
 end
 
 io.read()
 
-print("x is finally: " .. x)
-print("y is finally: " .. y)
+print("x is finally: " .. x) -- x is finally: 7
+print("y is finally: " .. y) -- y if finally: 22
 
 -- Boolean operations
 io.read()
 print("\nBooleans")
 
 local tru = true and true -- C: true && true
-print("tru: " .. tostring(tru))
+print("tru: " .. tostring(tru)) -- tru: true
 
 io.read()
 local fals = false or false -- C: true || false
-print("fals: " .. tostring(fals))
+print("fals: " .. tostring(fals)) -- fals: false
 
 io.read()
 local falsey = nil -- C: NULL
-print("not falsey: " .. tostring(not falsey)) -- C: !falsey
+print("not falsey: " .. tostring(not falsey)) -- not falsey: true (C: !falsey <=> Lua: not falsey)
 
 io.read()
 local notequal = falsey
-print("notequal: " .. tostring(true ~= notequal))
+print("notequal: " .. tostring(true ~= notequal)) -- notequal: true
 
 -- For loops
 io.read()
@@ -56,7 +55,7 @@ print("\nfor loop")
 --[[
   C: for (int i = a; i <= b; i += c) { }
   Lua: for i = a,b,c do end
-]]--
+]]
 
 print("\nLoop 1")
 io.read()
@@ -64,6 +63,13 @@ for i = 1,4 do
   print(i)
 end
 print("I declare a thumb war!")
+--[[
+1
+2
+3
+4
+I declare a thumb war!
+]]
 
 print("\nLoop 2")
 io.read()
@@ -71,6 +77,14 @@ for i = 5,1,-1 do
   print(i)
 end
 print("Blast off!")
+--[[
+5
+4
+3
+2
+1
+Blast off!
+]]
 
 print("\nLoop 3")
 io.read()
@@ -78,16 +92,26 @@ for i = 2,8,2 do
   print(i)
 end
 print("Who do we appreciate?!")
+--[[
+2
+4
+6
+8
+Who do we appreciate?!
+]]
 
 print("\nLoop 4")
 io.read()
 for i = 5,1,1 do
   print("[your favorite expletive here]")
 end
+--[[
+
+]]
 
 -- i is only in the scope of the for loops
 io.read()
-print("i: " .. tostring(i))
+print("i: " .. tostring(i)) -- i: nil
 
 -- While loop
 io.read()
@@ -96,14 +120,14 @@ print("\nwhile loop")
 --[[
   C: while (condition) { }
   Lua: while condition do end
-]]--
+]]
 
 local iterate_loop = true
 local foo = ""
 while #foo < 5 do
   foo = foo .. "ha"
 end
-print(foo)
+print(foo) -- hahaha
 
 io.read()
 print("\nif/elseif/else")
