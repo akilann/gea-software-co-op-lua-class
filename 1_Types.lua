7877c5fcc63223168fae3d19ab3e906ec70612dd
akilann <30810357+akilann@users.noreply.github.com>
2019-02-22 16:41:57 -0500

    typePrinter --> type_printer & comment corrections

--- 1_Types.lua
+++ 1_Types.lua
@@ -4,16 +4,16 @@ boolean = false
 number = 0
 string = "a"
 table = {}
-function typePrinter(name, variable)
+function type_printer(name, variable)
   print("Type of " .. name .. ": " .. type(variable))
 end
 
-typePrinter("`null`", null) -- Type of `null`: nil
-typePrinter("`boolean`", boolean) -- Type of `boolean`: Boolean
-typePrinter("`number`", number) -- Type of `number`: number
-typePrinter("`string`", string) -- Type of `string`: string
-typePrinter("`table`", table) -- Type of `table`: table
-typePrinter("`typePrinter`", typePrinter) -- Type of `typePrinter`: function
+type_printer("`null`", null) -- Type of `null`: nil
+type_printer("`boolean`", boolean) -- Type of `boolean`: Boolean
+type_printer("`number`", number) -- Type of `number`: number
+type_printer("`string`", string) -- Type of `string`: string
+type_printer("`table`", table) -- Type of `table`: table
+type_printer("`type_printer`", type_printer) -- Type of `type_printer`: function
 
 io.read()
 hex = 0xF1
@@ -26,22 +26,22 @@ print("`scientificNotation`: " .. scientificNotation) -- Type of `scientificNota
 
 io.read()
 table = 12345
-typePrinter("redefined `table`", table) -- Type of redefined `table`: number
+type_printer("redefined `table`", table) -- Type of redefined `table`: number
 
 io.read()
 num1 = "1"
 num2 = "2"
 num_result = num1 + num2
-typePrinter("`num1`", num1) -- Type of `num1`: string
-typePrinter("`num2`", num2) -- Type of `num2`: string
-typePrinter("`num_result`", num_result) -- Type of `num_result`: num_result
+type_printer("`num1`", num1) -- Type of `num1`: string
+type_printer("`num2`", num2) -- Type of `num2`: string
+type_printer("`num_result`", num_result) -- Type of `num_result`: number
 print(num1 .. " + " .. num2 .. " = " .. num_result) -- 1 + 2 = 3.0
 
 io.read()
 num1 = "1"
 num2 = 2
 num_result = num1 + num2
-typePrinter("`num1`", num1) -- Type of `num1`: string
-typePrinter("`num2`", num2) -- Type of `num2`: number
-typePrinter("`num_result`", num_result) -- Type of `num2`: number
+type_printer("`num1`", num1) -- Type of `num1`: string
+type_printer("`num2`", num2) -- Type of `num2`: number
+type_printer("`num_result`", num_result) -- Type of `num_result`: number
 print(num1 .. " + " .. num2 .. " = " .. num_result) -- 1 + 2 = 3.0
